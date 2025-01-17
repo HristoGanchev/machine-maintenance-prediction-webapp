f33c5b85e8cabbe28cec8eaab03d9182fa503509
Hristo Ganchev <hganchev@sensata.com>
2025-01-17 12:28:54 +0200

    add: Test for Machine Events

---
+++ MaintenancePrediction/MaintenancePrediction.Tests/API_Tests/MachineEvents/MachineEventsServiceTest.cs
@@ -0,0 +1,50 @@
+namespace MaintenancePrediction.Tests;
+using MaintenancePrediction.ApiService.Services;
+using MaintenancePrediction.ApiService.Models;
+using MaintenancePrediction.ApiService.Services;
+using MaintenancePrediction.ApiService.Models;
+using MaintenancePrediction.ApiService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[TestClass]
+public class MachineEventsServiceTest
+{
+    private MachineEventService _machineEventService;
+    private MachineMaintenanceDbContext _context;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        var options = new DbContextOptionsBuilder<MachineMaintenanceDbContext>()
+            .UseInMemoryDatabase(databaseName: "MachineMaintenanceTestDb")
+            .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+        _context = new MachineMaintenanceDbContext(options);
+        _context.Database.EnsureDeleted();
+        _machineEventService = new MachineEventService(_context);
+    }
+
+    [TestMethod]
+    public async void GetEventsAsync_ReturnsEvent()
+    {
+        // Arrange
+        _context.MachineEvents.AddRange(
+            new MachineEvent { EventId = 1, MachineId = 1, EventCode = "E101"},
+            new MachineEvent { EventId = 2, MachineId = 2, EventCode = "E102" }
+        );
+        _context.SaveChanges();
+
+        // Act
+        var result = await _machineEventService.GetEventsAsync(machineId: 1);
+
+        // Assert
+        Assert.AreEqual(1, result.Count());
+        Assert.AreEqual("E101", result.First().EventCode);
+    }
+}
